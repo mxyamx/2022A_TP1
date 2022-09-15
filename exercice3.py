8dd0da26aa46dd774d366e04be9340f22d25f737
maya <aitmayamanel@gmail.com>
2022-09-15 10:23:29 -0400

    exercice 3 resolu

--- exercice3.py
+++ exercice3.py
@@ -2,22 +2,34 @@
 def decomposer(secondes):
 
     # TODO: Assigner à la variable "annees" le nombre d'années
-    annees =
+    annees = int(secondes/31536000)
+    secondes= secondes-31536000*annees
+    print(annees, 'années')
 
     # TODO: Assigner à la variable "semaines" le nombre de semaines restantes
-    semaines =
+    semaines = int(secondes / 604800)
+    secondes = secondes-604800*semaines
+    print(semaines, 'semaines')
 
     # TODO: Assigner à la variable "jours" le nombre de jours restants
-    jours =
+    jours = int(secondes / 86400)
+    secondes = secondes-86400*jours
+    print(jours, 'jours')
 
     # TODO: Assigner à la variable "heures" le nombre d'heures restantes
-    heures =
+    heures = int(secondes / 3600)
+    secondes = secondes - 3600*heures
+    print(heures, 'heures')
 
     # TODO: Assigner à la variable "minute" le nombre de minutes restantes
-    minutes =
+    minutes = int(secondes / 60)
+    secondes = secondes - 60*minutes
+    print(minutes, 'minutes')
 
     # TODO: Assigner à la variable "secondes" le nombre de secondes restantes
-    secondes =
+    secondes = int(secondes)
+    reste = secondes
+    print(secondes, 'secondes')
 
     # TODO: Afficher le nombres d'années, semaines, jours, heures, minutes et secondes
     print(annees ,semaines ,jours ,heures ,minutes ,secondes)
